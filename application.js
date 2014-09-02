2ecc433a15384a2448d91507b3c72568b4e812b3
Spencer Zhang <syz4@cornell.edu>
2014-09-02 15:46:42 -0500

    Implemented player controls

--- application.js
+++ application.js
@@ -1,3 +1,46 @@
+var gameStarted
+var currentPlayer
+var board
+
+
+
+function resetGame() {
+  board = Array(9);
+  gameStarted = true;
+  currentPlayer = "O";
+}
+
+function clickBox(boxIndex) {
+  if(board[boxIndex] == undefined) {
+    board[boxIndex] = currentPlayer
+    nextPlayer();
+  }
+}
+
+function nextPlayer() {
+  if(currentPlayer == "X") {
+    currentPlayer = "O";
+  } else {
+    currentPlayer = "X";
+  }
+}
+
+function printBoard() {
+  for(i in board){
+    if(board[i] != undefined) {
+      $("#b" + i).text(board[i])
+    }
+  }
+}
+
+
+
 $(function() {
-  console.log('test');
+  resetGame();
+
+  $('.box').click(function() {
+    boxIndex = parseInt($(this).attr('id')[1]);
+    clickBox(boxIndex);
+    printBoard();
+  })
 })
