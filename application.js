1c7c9fe498033cd852afdfb872626a048df766b6
Spencer Zhang <syz4@cornell.edu>
2014-09-02 16:26:55 -0500

    Implemented AI logic

--- application.js
+++ application.js
@@ -2,6 +2,17 @@ var gameStarted
 var currentPlayer
 var board
 
+var PATHS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+];
+
 
 
 function resetGame() {
@@ -17,7 +28,34 @@ function clickBox(boxIndex) {
   }
 }
 
+function checkVictoryConditions() {
+  var index, numO, numX, path, j, box
+  for(index in PATHS) {
+    numO = 0;
+    numX = 0;
+    path = PATHS[index];
+    valueChange = 0;
+
+    for(j in path) {
+      box = path[j];
+      if(board[box] == "O") {
+        numO += 1;
+      }
+      if(board[box] == "X") {
+        numX += 1;
+      }
+    }
+
+    if(numO == 3 || numX == 3) {
+      gameStarted = false;
+
+    }
+
+  }
+}
+
 function nextPlayer() {
+  checkVictoryConditions()
   if(currentPlayer == "X") {
     currentPlayer = "O";
   } else {
@@ -37,14 +75,80 @@ function printBoard() {
 }
 
 
+function computerMove() {
+  // The Computer calculates the value of each position on the board.
+  // The center of the board is worth the most, and corners are worth more than sides.
+  var numO, numX, valueChange, boxIndex, index
+  var valueArray = [5, 0, 5, 0, 10, 0, 5, 0, 5];
+
+  // The computer checks each row, column, and diagonal
+
+  for(index in PATHS) {
+    numO = 0;
+    numX = 0;
+    path = PATHS[index];
+    valueChange = 0;
+
+    for(j in path) {
+      box = path[j];
+      if(board[box] == "O") {
+        numO += 1;
+      }
+      if(board[box] == "X") {
+        numX += 1;
+      }
+    }
+
+    if(numO + numX < 3) {
+      // If it can win on the next turn, the most valuable move is the winning move.
+      if(numX == 2) {
+        valueChange = 500;
+      }
+
+      // Otherwise, if the player can win, the most valuable move is one that blocks the player.
+      if(numO == 2) {
+        valueChange = 200;
+      }
+
+      // If a path contains one X and one O, its value goes down.
+      if(numO == 1 && numX == 1) {
+        valueChange = -10;
+      }
+    }
+
+    for(j in path) {
+      box = path[j]
+      valueArray[box] += valueChange;
+    }
+  }
+
+  console.log(valueArray)
+
+  // Go through the value array and find the box position with the highest value
+  boxIndex = 0
+  for(index = 0; index < 9; index++) {
+    if(board[index] != undefined) { valueArray[index] = -500 }
+    if(valueArray[index] > valueArray[boxIndex]) {
+      boxIndex = index;
+    }
+  }
+
+  clickBox(boxIndex);
+  printBoard();
+}
+
+
 
 $(function() {
   resetGame();
 
   $('.box').click(function() {
-    boxIndex = parseInt($(this).attr('id')[1]);
-    clickBox(boxIndex);
-    printBoard();
+    if(gameStarted == true && currentPlayer == "O") {
+      boxIndex = parseInt($(this).attr('id')[1]);
+      clickBox(boxIndex);
+      printBoard();
+      computerMove();
+    }
   })
 
   $('button').click(function() {
