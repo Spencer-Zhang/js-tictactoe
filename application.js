193276ac59d202abaf69148af7c1ac3867c88105
Spencer Zhang <syz4@cornell.edu>
2014-09-02 15:55:19 -0500

    Added reset button

--- application.js
+++ application.js
@@ -26,9 +26,12 @@ function nextPlayer() {
 }
 
 function printBoard() {
-  for(i in board){
+  var i
+  for(i=0; i<9; i++){
     if(board[i] != undefined) {
       $("#b" + i).text(board[i])
+    } else {
+      $("#b" + i).text('')
     }
   }
 }
@@ -41,6 +44,11 @@ $(function() {
   $('.box').click(function() {
     boxIndex = parseInt($(this).attr('id')[1]);
     clickBox(boxIndex);
+    printBoard();
+  })
+
+  $('button').click(function() {
+    resetGame();
     printBoard();
   })
 })
